fa04ccbfc32040c360136251c91bbbe26939a95b
Jon Merrifield <jon@jmerrifield.com>
2015-01-02 11:59:10 -0900

    Introduce blackbox tests for happy-path restart and restart after a failed worker (failing)

---
+++ test/support/helpers.js
@@ -0,0 +1,54 @@
+var request = require('request')
+  , csp = require('js-csp')
+  , transducers = require('transducers.js')
+  , spawn = require('child_process').spawn
+  , JSONStream = require('JSONStream')
+  , immutable = require('immutable')
+  , filter = transducers.filter
+  , tap = csp.operations.mult.tap
+
+// Initiate an HTTP GET request to the specified URL. Returns a channel that
+// will have a map placed on it containing the response status and body
+module.exports.httpGetToChan = function (url) {
+  var ch = csp.chan()
+
+  request.get(url, {json: {}}, (err, res, body) => {
+    if (err) {
+      return csp.putAsync(ch, immutable.fromJS({err: err}))
+    }
+
+    csp.putAsync(ch, immutable.fromJS({status: res.statusCode, body: body}))
+  })
+
+  return ch
+}
+
+// Returns a channel that will receive log messages from the specified mult
+// (which issues JSON log entries) matching the specified message.
+module.exports.waitForLogMessage = function (message, mult) {
+  return tap(mult, csp.chan(1, filter(x => x.msg === message)))
+}
+
+// Start the dutch-master module. Returns a map containing the PID of the master
+// process, and a channel that will have log messages from the cluster placed
+// upon it in JSON format (obtained by piping the masters STDOUT into a streaming
+// JSON parser).
+module.exports.startCluster = function (env) {
+  var nodeCmd = process.execPath
+    , args = [__dirname + '/master-shim.js']
+    , stdoutCh = csp.chan()
+
+  var ps = spawn(nodeCmd, args, {
+    env: env
+  , stdio: [null, 'pipe', process.stderr]
+  })
+
+  ps.stdout
+  .pipe(JSONStream.parse())
+  .on('data', data => csp.putAsync(stdoutCh, data))
+
+  return {
+    stdoutCh: stdoutCh
+  , pid: ps.pid
+  }
+}
---
+++ test/support/worker.js
@@ -0,0 +1,82 @@
+// This fake worker gets invoked by dutch-master, and performs actions we tell
+// it to via communications with the worker-coordinator.
+
+// This is not run through the 6to5 compiler so no ES6 syntax is available.
+
+// All stdout output from the master process (and hence, this script) will be
+// parsed by a streaming JSON parser to interpret log output. If you need to add
+// logging in here for diagnostic purposes, use `console.error`.
+
+var request = require('request')
+  , when = require('when')
+  , cluster = require('cluster')
+  , immutable = require('immutable')
+
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , coordinatorUrl = 'http://localhost:' + process.env.COORDINATOR_PORT
+  , workerInfoUrl = coordinatorUrl + '/worker/' + cluster.worker.id
+  , sharedState = immutable.Map()
+
+function ensureLongReqState() {
+  if (!sharedState.has('longRequestDefer')) {
+    sharedState = sharedState.set('longRequestDefer', when.defer())
+  }
+
+  return sharedState.get('longRequestDefer')
+}
+
+process.on('SIGTERM', function () {
+  request.post(workerInfoUrl + '/signal/SIGTERM', {json: {}}, function () {
+    process.exit(143)
+  })
+})
+
+// Simple happy-path endpoint
+app.get('/', function (req, res) {
+  res.json({workerId: cluster.worker.id})
+})
+
+// This endpoint will not complete until we receive an instruction to do so
+app.get('/long-request', function (req, res) {
+  ensureLongReqState().promise.done(function () {
+    sharedState = sharedState.delete('longRequestDefer')
+    res.json({
+      workerId: cluster.worker.id
+    })
+  })
+})
+
+// Notify the coordinator that we are running
+request.post(workerInfoUrl + '/running', waitForInstruction)
+
+// Long-poll for instructions
+function waitForInstruction() {
+  request.get(workerInfoUrl, {json: {}}, function (err, res, body) {
+    if (err) return
+
+    if (body.action === 'startListening' && !sharedState.get('listening')) {
+      sharedState = sharedState.set('listening', true)
+
+      server.listen(null, function () {
+        request.post(coordinatorUrl + body.completionUrl, {
+          json: {
+            clusterPort: server.address().port
+          }
+        })
+      })
+    }
+
+    if (body.action === 'completeLongRequest') {
+      request.post(coordinatorUrl + body.completionUrl)
+      ensureLongReqState().resolve()
+    }
+
+    if (body.action === 'crash') {
+      request.post(coordinatorUrl + body.completionUrl)
+      process.exit(1)
+    }
+
+    waitForInstruction()
+  })
+}
