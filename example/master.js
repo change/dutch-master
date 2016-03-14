c2103cc21ab67c1be60432d0cc576c5c6bcc9340
Jon Merrifield <jon@jmerrifield.com>
2016-03-14 14:31:10 -0800

    Update example master.js

--- example/master.js
+++ example/master.js
@@ -1,11 +1,17 @@
-require('dutch-master').start({
-  worker: 'app.js',
+require('..')({
+  worker: 'worker.js',
   beforeFork: function () {
     process.chdir('/deploys/app/current')
   },
+
+  // Calculate number of workers at startup
   numWorkers: function () {
     return Math.max(require('os').cpus().length, 2)
   },
+
+  // Or use a static number
+  // numWorkers: 2
+
   logger: require('bunyan')({name: 'my-app'}),
   workerEnvironment: {
     NODE_ENV: 'production'
